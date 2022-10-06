bcc2ec94203d55ebd6be4c2230fd083be3b5144d
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-06 20:05:01 +0800

    feed method

--- interpreter/tests/test.nim
+++ interpreter/tests/test.nim
@@ -1,12 +1,24 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
+import Quikskript/interpreter
 
-import Quikskript
-test "can add":
-  check add(5, 5) == 10
+## This test tests if ``Interpreter.feed()`` is working.
+test "feed test":
+    var itr: Interpreter = initInterpreter()
+    
+    # probably cant see it but this string is full of yucky whitespace
+    let test_string: string = """
+    Hello world!
+    The itr.feed() method converts a string full of
+        whitespace and cleans it up by     
+
+stripping the string and checking if it's pure empty.   
+    """
+
+    var feed: seq[string] = itr.feed(test_string)
+
+    check feed == @[
+        "Hello world!",
+        "The itr.feed() method converts a string full of",
+        "whitespace and cleans it up by",
+        "stripping the string and checking if it\'s pure empty."
+    ]
