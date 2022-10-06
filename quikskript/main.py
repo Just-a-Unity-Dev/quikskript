ffef486fc29e3473ee8102ca3fe5cb7348177480
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-06 08:47:57 +0800

    typechecking and stuff

--- quikskript/main.py
+++ quikskript/main.py
@@ -1,18 +1,33 @@
 # quikskript - 2022
 # written by eclipsedotspace@gmail.com
 # hosted under the MIT license
-# written under 100 lines in Python
-# https://github.com/Just-a-Unity-Dev/quikskript
-# eclips-e.space
+# eclips-e.space | https://github.com/Just-a-Unity-Dev/quikskript
 
 class Interpreter():
     def __init__(self):
         self.commands = {}
         self.funcs = {}
+        self.vars = {}
     
     def add_command(self, name, func):
         self.commands.__setitem__(name, func)
 
+    def set_var(self, name, val, vtype):
+        if vtype == "s":
+            self.vars.__setitem__(name, val)
+        elif vtype == "i":
+            self.vars.__setitem__(name, int(val))
+        elif vtype == "f":
+            self.vars.__setitem__(name, float(val))
+        elif vtype == "b":
+            self.vars.__setitem__(name, val == 'true')
+    
+    def get_var(self, name):
+        try:
+            return self.vars.__getitem__(name)
+        except KeyError:
+            return None
+    
     def feed(self, rstring: str):
         strings = rstring.split("\n")
         for string in strings:
@@ -61,31 +76,83 @@ class Interpreter():
                     raise Exception()
             else:
                 # not a thing
-                # raise Exception()
-                pass
+                raise Exception()
 
 test = """
-* ooooohh look at me so spooky comment
-/setup/
-    out test1 test2 test3
+* quik is an assembly/sql-like language, following a (command) (arg) syntax
+* this is a comment lol
+* these are global variables, always declared at a top of a file
+* global i 0
+
+* below is a function declared by starting and ending an identifier with /
+/func/
+    * order of functions is important
+    * you cant access a func declared below you
+    set i i 0
+    out *i
+    call /func/ > i 10
     done
 
-*laugh
 /main/
-    call /setup/
-    done
+    * the main function is recommended to be put last of all other funcs so that /main/ has full access to all other funcs
+    call /func/ ?
+    out the function FINALLY finished
+    out hello world!
 """
 
-itr = Interpreter()
+interpreter = Interpreter()
 
 def call_command(args, itr: Interpreter):
     func = itr.funcs[args[0][1:]]
-    itr.commands[func[0]](func[1])
+    params = args[1]
+    
+    evaluated = False
+    inverted = False
+    var = None
+    cmp = None
+    if not params == "?":
+        var = itr.get_var(args[2])
+        cmp = args[3]
+        if args[3].startswith("*"):
+            cmp = itr.get_var(args[3][1:])
+    else:
+        evaluated = True
 
-def out_command(args):
-    print(' '.join(args))
+    if params == "!":
+        # inverse
+        inverted = not inverted
+    elif params == ">":
+        print(var, cmp, var > cmp)
+        if var > cmp:
+            evaluated = True
+    elif params == "<":
+        if var < cmp:
+            evaluated = True
+    elif params == "=":
+        if var == cmp:
+            evaluated = True
 
-itr.add_command("call", call_command)
-itr.add_command("out", out_command)
+    if inverted:
+        evaluated = not evaluated
 
-itr.interpret(test)
+    if evaluated:
+        for command in func:
+            itr.commands[func[0]](func[1], itr)
+            del func[:2]
+
+def out_command(args, ir: Interpreter):
+    if args[0].startswith("*"):
+        print(ir.get_var(args[0][1:]))
+    else:
+        print(' '.join(args))
+
+def set_command(args, ir: Interpreter):
+    var_name = args.pop(0)
+    var_type = args.pop(0)
+    ir.set_var(var_name, ' '.join(args), var_type)
+
+interpreter.add_command("call", call_command)
+interpreter.add_command("out", out_command)
+interpreter.add_command("set", set_command)
+
+interpreter.interpret(test)
