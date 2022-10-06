780ad0f55efa784a00373ad111c5a6adf0d3e0fe
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-06 15:19:23 +0800

    CLI "functionality"

--- cli/src/cli.nim
+++ cli/src/cli.nim
@@ -1,5 +1,44 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+# cli.nim
+import std/strformat
+import parseopt, os
+
+proc writeHelp() =
+  echo """
+  usage: <operation> [...]
+  operations:
+    -h | --help           : show's this message
+    -v | --version        : show's the cli version
+    -f | --file           : reads from a file and interpret's it
+  """
+
+proc writeVersion() =
+  echo &"cli v0.0.1\n"
+
+proc cli*() =
+  if paramCount() == 0:
+    writeHelp()
+    quit(0)
+  
+  for kind, key, val in getopt():
+    case kind
+    of cmdLongOption, cmdShortOption:
+      case key:
+        of "help", "h":
+          writeHelp()
+          quit(0)
+        of "version", "v":
+          writeVersion()
+          quit(0)
+        of "file", "f":
+          # TODO: Add functionality to the CLI when the interpreter works
+          # Take the CL args, read the file, handle errors
+          # Then feed into interpreter or something.
+          echo "Not Finished"
+          quit(1)
+        else:
+          discard
+    else:
+      discard
 
 when isMainModule:
-  echo("Hello, World!")
+  cli()
