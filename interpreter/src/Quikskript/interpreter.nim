c492718b32f78be063a3bc40b378529ac3a1167b
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-06 20:27:52 +0800

    formatting

--- interpreter/src/Quikskript/interpreter.nim
+++ interpreter/src/Quikskript/interpreter.nim
@@ -2,24 +2,24 @@
 import std/tables
 import strutils
 
-type GenericEnum = enum
-    s,i,b
+type GenericTypeEnum = enum
+  s,i,b
 
 type Interpreter* = object
-    ## The interpreter object contains all the code to feed, convert and run quik! code. Initialize with the ``initializeInterpreter`` func.
-    vars*: Table[string, Table[string, GenericEnum]]
+  ## The interpreter object contains all the code to feed, convert and run quik! code. Initialize with the ``initializeInterpreter`` func.
+  vars*: Table[string, Table[string, GenericTypeEnum]]
 method feed*(self: Interpreter, str: string): seq[string]{.base.} =
-    var foods: seq[string] = str.split("\n")
-    var i: int32 = 0;
-    while i < foods.len:
-        let food: string = foods[i]
-        var strip: string = food.unindent().strip()
-        foods[i] = strip
-        if strip.isEmptyOrWhitespace():
-            foods.del(i)
-            i -= 1
-        i += 1
-    return foods
+  var foods: seq[string] = str.split("\n")
+  var i: int32 = 0;
+  while i < foods.len:
+      let food: string = foods[i]
+      var strip: string = food.unindent().strip()
+      foods[i] = strip
+      if strip.isEmptyOrWhitespace():
+          foods.del(i)
+          i -= 1
+      i += 1
+  return foods
 
 proc initInterpreter*(): Interpreter =
-    Interpreter()
+  Interpreter()
--- interpreter/tests/test.nim
+++ interpreter/tests/test.nim
@@ -3,22 +3,22 @@ import Quikskript/interpreter
 
 ## This test tests if ``Interpreter.feed()`` is working.
 test "feed test":
-    var itr: Interpreter = initInterpreter()
-    
-    # probably cant see it but this string is full of yucky whitespace
-    let test_string: string = """
+  var itr: Interpreter = initInterpreter()
+  
+  # probably cant see it but this string is full of yucky whitespace
+  let test_string: string = """
     Hello world!
     The itr.feed() method converts a string full of
         whitespace and cleans it up by     
 
 stripping the string and checking if it's pure empty.   
-    """
+  """
 
-    var feed: seq[string] = itr.feed(test_string)
+  var feed: seq[string] = itr.feed(test_string)
 
-    check feed == @[
-        "Hello world!",
-        "The itr.feed() method converts a string full of",
-        "whitespace and cleans it up by",
-        "stripping the string and checking if it\'s pure empty."
-    ]
+  check feed == @[
+    "Hello world!",
+    "The itr.feed() method converts a string full of",
+    "whitespace and cleans it up by",
+    "stripping the string and checking if it\'s pure empty."
+  ]
